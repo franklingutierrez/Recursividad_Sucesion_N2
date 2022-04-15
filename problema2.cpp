3f3587b3c2f3a4247924852f7fd698357a207bae
guiratec <fgutierrezarizaca@gmail.com>
2022-04-15 11:41:26 -0500

    Finalizado 2

--- problema2.cpp
+++ problema2.cpp
@@ -1,4 +1,4 @@
-/*Mostrar los n primeros términos de la sucesión: 1,3,4,7,11....., Xn; n , es ingresado por pantalla.
+/*5. Mostrar los n primeros términos de la sucesión: 1,3,4,7,11....., Xn; n , es ingresado por pantalla.
 Autor --> Franklin Hiustong Gutierrez Arizaca*/
 #include <iostream>
 using namespace std;
@@ -18,6 +18,5 @@ int main() {
     cout<<"ingrese una cantidad [mayor a 0] para calcular la sucesion: "; cin>>x;
     cout<<"1 "<<"3 ";
     sucesion(1,3,x);
-
     return 0;
 }
